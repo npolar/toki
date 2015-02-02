5a6bfb0373be46d2a1d3b1a390bedca00626cbb9
RDuX <ruben.dens@gmail.com>
2015-02-02 18:46:36 +0100

    Doh! Do not use constants for the algorithms. Redefined them as functions returning a pointer to the Algorithm type

--- algorithm.go
+++ algorithm.go
@@ -4,25 +4,31 @@ import (
 	"crypto"
 )
 
-const (
-	HS256 = Algorithm{
-		Name:      "HS256",
-		Singature: crypto.SHA256,
-	}
-
-	HS384 = Algorithm{
-		Name:      "HS384",
-		Singature: crypto.SHA384,
-	}
-
-	HS512 = Algorithm{
-		Name:      "HS512",
-		Singature: crypto.SHA512,
-	}
-)
-
 type Algorithm struct {
 	Name      string      // Algorithm name as specified in the JWA definition: https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
 	Singature crypto.Hash // Algorithm to be used when creating a JWS Object
 	Crypto    crypto.Hash // Algorithm to be used when generating a JWE Object
+}
+
+// Define the supported SHA2 Algorithms used in the JWS
+
+func HS256() *Algorithm {
+	return &Algorithm{
+		Name:      "HS256",
+		Singature: crypto.SHA256,
+	}
+}
+
+func HS384() *Algorithm {
+	return &Algorithm{
+		Name:      "HS384",
+		Singature: crypto.SHA384,
+	}
+}
+
+func HS512() *Algorithm {
+	return &Algorithm{
+		Name:      "HS512",
+		Singature: crypto.SHA512,
+	}
 }
