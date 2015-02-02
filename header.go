7ebd3125cfa5b2c753b364847e2b3dc70de4c8b2
RDuX <ruben.dens@gmail.com>
2015-02-02 17:59:14 +0100

    Defined the Header type. Added methods to convert the header to Json and UrlEncoded base64

---
+++ header.go
@@ -0,0 +1,31 @@
+package toki
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
+// Define a new struct for the JWT JOSE header. field definitions can be found here:
+// https://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-40#section-4.1
+type Header struct {
+	Typ string
+	Alg string
+	Enc string
+	Zip string
+	Kid string
+}
+
+// JsonHeader encodes the Header struct into a json []byte.
+func (header *Header) JsonHeader() ([]byte, error) {
+	raw, err := json.Marshal(header)
+	return raw, err
+}
+
+// Base64Header generates a URLencoded base64 string from the JsonHeader.
+// See (header *Header)JsonHeader for details on the json encoding.
+func (header *Header) Base64Header() (string, error) {
+	headerJson, err := header.JsonHeader()
+	return base64.URLEncoding.EncodeToString(headerJson), err
+}
