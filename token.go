2ed997811c20181ed9469a731df89cbd4471629c
RDuX <ruben.dens@gmail.com>
2015-02-09 15:42:47 +0100

    refactored struct to use names more compliant with the JWT spec. Added methods for generating JoseClaim segment and full token string to the caller. Added method that generates a Json Web signature

--- token.go
+++ token.go
@@ -1,38 +1,80 @@
 package toki
 
 import (
+	"errors"
 	"strings"
 )
 
 type JsonWebToken struct {
-	Payload        *Claims    // The contents of the claims segment defined in JWT
-	String         string     // Contains the full JWT object as a string
+	Jose           *Header    // Info @ toki/header.go
+	Claim          *Claims    // Info @ toki/claims.go
+	Signature      string     // The signature HMAC of the base64 encoded header.claim
 	TokenAlgorithm *Algorithm // Specifies the algorithm used by the token
 }
 
 func NewJsonWebToken() *JsonWebToken {
 	return &JsonWebToken{
-		TokenAlgorithm: HS256(), // Use the HS256 algorithm by default
+		Jose:           NewHeader(), // Set the default header to match the default algorithm
+		TokenAlgorithm: HS256(),     // Use the HS256 algorithm by default
+		Claim:          NewClaims(), // Initialize the body with a new claims object
 	}
 }
 
-// GenerateHeader reads the contents of the SigningAlg and EncryptionAlg keys and
-// generates a JWT/JWE compliant JOSE header. Function returns a pointer to a the
-// toki Header type offering additional encoding options.
-func (jwt *JsonWebToken) GenerateHeader() *Header {
-	var header Header
-
-	// When the TokenAlgorithm doesn't specify Crypto it means you have a JWT.
-	// When crypto is present then you have a JWE enabled token
-	if jwt.TokenAlgorithm.Crypto.Available() {
-		header.Typ = "JWT"
-		header.Alg = jwt.TokenAlgorithm.Name
+// UpdateTokenHeader uses SigningAlg and EncryptionAlg to update the tokenType and algorithm
+// settings of the header so it matches the specified TokenAlgorithm.
+func (jwt *JsonWebToken) UpdateTokenHeader() {
+	// When the TokenAlgorithm specifies crypto we have a JWE token.
+	if jwt.TokenAlgorithm.CryptoHash.Available() {
+		jwt.Jose.TokenType = "JWE"
+		jwt.Jose.Algorithm = jwt.TokenAlgorithm.Name
 	} else {
-		header.Typ = "JWE"
-		header.Alg = jwt.TokenAlgorithm.Name
+		jwt.Jose.TokenType = "JWT"
+		jwt.Jose.Algorithm = jwt.TokenAlgorithm.Name
+	}
+}
+
+// Sign uses the tokens header and claims contents to generate a signature for the token.
+func (jwt *JsonWebToken) Sign(secret string) error {
+	jwt.UpdateTokenHeader() // Update the header to match any overriden values
+
+	if content, err := jwt.JoseClaimString(); err == nil {
+		base64Hmac := jwt.TokenAlgorithm.Base64Hmac(secret, content)
+		jwt.Signature = StripBase64Padding(base64Hmac)
+	} else {
+		return err
 	}
 
-	return &header
+	return nil
+}
+
+// String combines Content String and the result from a S returns the full JWT string
+func (jwt *JsonWebToken) String() (string, error) {
+	if jwt.Signature != "" {
+		content, err := jwt.JoseClaimString()
+
+		if err != nil {
+			return "", err
+		}
+
+		return content + "." + jwt.Signature, err
+	} else {
+		// Optionally set the header to unsecure and don't use a secret to sign!
+		return "", errors.New("Token is missing a signature! Sign the token before calling string")
+	}
+}
+
+// JoseClaimString joins the base64 of the Jose header and claim with a dot
+func (jwt *JsonWebToken) JoseClaimString() (string, error) {
+	encodedClaims, err := jwt.Claim.Base64()
+	encodedHeader, err := jwt.Jose.Base64()
+
+	// If one of both generates an error return a blank string with the error.
+	// Since the err var is re-used the last error will bleed through first.
+	if err != nil {
+		return "", err
+	}
+
+	return encodedHeader + "." + encodedClaims, err
 }
 
 // StripBase64Padding strips the base64 padding char (=) from the provided content string.
