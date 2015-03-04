df23e52f7bf6ad9b863b0ea6d81c36c9d0157c85
RDuX <ruben.dens@gmail.com>
2015-03-04 11:05:30 +0100

    Fixed a bug with the parse function. State wasn't being written to the Header correctly. Instead of using an extra variable I solved it by decoding directly into the Header struct

--- header.go
+++ header.go
@@ -13,7 +13,7 @@ type Header struct {
 	Alg string `json:"alg,omitempty"` // Token Alg [Determined by the algorithm] - REQUIRED
 	Enc string `json:"enc,omitempty"` // Token Encryption [Determined by the algorithm] - OPTIONAL
 	Zip string `json:"zip,omitempty"` // Compression used on the token - OPTIONAL
-	Kid string `json:"kid,omitempty"` // Unique token ID - OPTIONAL
+	Kid string `json:"kid,omitempty"` // Hint as to which key was used to sign the token - OPTIONAL
 }
 
 // NewHeader creates a header object with the default algorithm settings
@@ -38,22 +38,15 @@ func (header *Header) Base64() (string, error) {
 	return StripBase64Padding(encodedString), err
 }
 
+// Parse the json header into a Header object and check if it's valid
 func (header *Header) Parse(jose string) error {
-	var decoded Header
+	err := json.Unmarshal([]byte(jose), header)
+	err = header.Valid()
 
-	if err := json.Unmarshal([]byte(jose), &decoded); err != nil {
-		return err
-	}
-
-	if err := decoded.Valid(); err != nil {
-		return err
-	}
-
-	header = &decoded
-
-	return nil
+	return err
 }
 
+// Valid checks if the minimal header requirements are met
 func (header *Header) Valid() error {
 	if header.Alg == "" {
 		return errors.New("[Invalid Jose Header] Missing required field: alg")
@@ -61,19 +54,3 @@ func (header *Header) Valid() error {
 
 	return nil
 }
-
-// @TODO Add checks for the supported types so we can bail on corrupted header contents
-//func (header *Header) ValidType(typ string) bool {
-//
-//}
-//
-//func (header *Header) ValidAlg(alg string) bool {
-//
-//}
-//
-//// ValidEncryption checks if the encryption name
-//// matches one of the types defined by the JWA spec
-//// https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-5
-//func (header *Header) ValidEncryption(enc string) bool {
-//
-//}
